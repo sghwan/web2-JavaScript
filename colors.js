beef4a26fe4fbd27e6cfaf860a0d577d9677d4ea
sghwan <78450157+sghwan@users.noreply.github.com>
2021-02-16 19:42:27 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -20,17 +20,44 @@ var Body = {
   }
 }
 
+var Ol = {
+  setLineColor: function(color) {
+    document.querySelector('ol').style.borderRightColor = color;
+    //$('body').css('color', color);
+  }
+}
+
+var H1 = {
+  setLineColor: function(color) {
+    document.querySelector('h1').style.borderBottomColor = color;
+    //$('body').css('color', color);
+  }
+}
+
+var Line = {
+  setLineColor: function(color) {
+    document.querySelector('#line').style.borderRightColor = color;
+    //$('body').css('color', color);
+  }
+}
+
 function nightDayHandler(self) {
 
   if (self.value === 'night') {
     Body.setColor('white');
     Body.setBackgroundColor('black');
     Links.setColor('white');
+    Ol.setLineColor('white');
+    H1.setLineColor('white');
+    Line.setLineColor('white');
     self.value = 'day';
   } else {
     Body.setColor('black');
     Body.setBackgroundColor('white');
     Links.setColor('black');
+    Ol.setLineColor('black');
+    H1.setLineColor('black');
+    Line.setLineColor('black');
     self.value = 'night';
   }
 }
